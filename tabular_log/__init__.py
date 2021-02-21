973cb8b7956587b03fa1e368456cbe3beb44b65f
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-21 17:25:57 +0100

    Created tabular_log/__init__.py

---
+++ tabular_log/__init__.py
@@ -0,0 +1,19 @@
+"""tabular_log
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-21"
+
+class tabular_log:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(tabular_log.tabular_log())
+	
+	def tabular_log():
+		"""tabular_log first funtion
+		"""
+		return "tabular_log" #for the test (see test_tabular_log.py)
+		
+if __name__ == "__main__":
+	tabular_log()
