028269ea91d11bc56d429b3bb747e2db765d9bed
DavideC03 <davide.castellani.5.8.03@gmail.com>
2021-02-21 18:13:58 +0100

    Fixed a bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from requests import get
 
 setup(
-      name='tabular_log-castellani-davide',
+      name='tabular_log',
       version=get("https://api.github.com/repos/CastellaniDavide/tabular_log/tags").json()[0]['name'].replace("v", "") if len(get("https://api.github.com/repos/CastellaniDavide/tabular_log/tags").json()) > 0 else "0.0", # Lastest release
       description=get("https://api.github.com/repos/CastellaniDavide/tabular_log").json()['description'],
       long_description=get("https://raw.githubusercontent.com/CastellaniDavide/tabular_log/master/docs/README.md").text,
